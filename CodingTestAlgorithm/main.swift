6afc3921c2086bafecd8a7405b7eec32c25fc76d
Guboneui <starku2249@naver.com>
2022-01-04 16:02:01 +0900

    프로그래머스 LV1 행렬의 덧셈

--- CodingTestAlgorithm/main.swift
+++ CodingTestAlgorithm/main.swift
@@ -8,18 +8,50 @@
 import Foundation
 
 // MARK: - LV1 직사각형 별찍기
-let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
-let (a, b) = (n[0], n[1])
+//let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+//let (a, b) = (n[0], n[1])
+//
+//// Array joined()를 통한 풀이
+//// for _ in 0..<b {
+////     print(Array(repeating: "*", count: a).joined())
+//// }
+//
+//// print의 terminator 디폴트 값 변경을 통한 풀이
+//for _ in 1...b {
+//    for _ in 1...a {
+//        print("*", terminator: "")
+//    }
+//    print("")
+//}
 
-// Array joined()를 통한 풀이
-// for _ in 0..<b {
-//     print(Array(repeating: "*", count: a).joined())
-// }
+// MARK: - LV1 행렬의 덧셈
+let arr1 = [[1,2],[2,3]]
+let arr2 = [[3,4],[5,6]]
 
-// print의 terminator 디폴트 값 변경을 통한 풀이
-for _ in 1...b {
-    for _ in 1...a {
-        print("*", terminator: "")
+var arr3: [[Int]] = Array(repeating: Array(repeating: 0, count: arr1[0].count), count: arr1.count)
+
+for i in 0..<arr3.count {
+    for j in 0..<arr3[0].count {
+        arr3[i][j] = arr1[i][j] + arr2[i][j]
     }
-    print("")
+}
+print(arr3)
+
+let k = zip(arr1, arr2).map{zip($0, $1).map{$0 + $1}}
+print(k)
+
+ func solution1(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
+    
+     return zip(arr1, arr2).map{zip($0, $1).map{$0 + $1}}
+ }
+
+func solution2(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
+    var newArr: [[Int]] = Array(repeating: Array(repeating: 0, count: arr1[0].count), count: arr1.count)
+    for i in 0..<newArr.count {
+        for j in 0..<newArr[0].count {
+            newArr[i][j] = arr1[i][j] + arr2[i][j]
+        }
+    }
+    
+    return newArr
 }
