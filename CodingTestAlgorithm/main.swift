8997d92d7ca12dedac026507a555ec774996b198
Guboneui <starku2249@naver.com>
2022-01-04 16:37:35 +0900

    프로그래머스 LV1 평균 구하기

--- CodingTestAlgorithm/main.swift
+++ CodingTestAlgorithm/main.swift
@@ -25,33 +25,40 @@ import Foundation
 //}
 
 // MARK: - LV1 행렬의 덧셈
-let arr1 = [[1,2],[2,3]]
-let arr2 = [[3,4],[5,6]]
+//let arr1 = [[1,2],[2,3]]
+//let arr2 = [[3,4],[5,6]]
+//
+//var arr3: [[Int]] = Array(repeating: Array(repeating: 0, count: arr1[0].count), count: arr1.count)
+//
+//for i in 0..<arr3.count {
+//    for j in 0..<arr3[0].count {
+//        arr3[i][j] = arr1[i][j] + arr2[i][j]
+//    }
+//}
+//print(arr3)
+//
+//let k = zip(arr1, arr2).map{zip($0, $1).map{$0 + $1}}
+//print(k)
+//
+// func solution1(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
+//
+//     return zip(arr1, arr2).map{zip($0, $1).map{$0 + $1}}
+// }
+//
+//func solution2(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
+//    var newArr: [[Int]] = Array(repeating: Array(repeating: 0, count: arr1[0].count), count: arr1.count)
+//    for i in 0..<newArr.count {
+//        for j in 0..<newArr[0].count {
+//            newArr[i][j] = arr1[i][j] + arr2[i][j]
+//        }
+//    }
+//
+//    return newArr
+//}
 
-var arr3: [[Int]] = Array(repeating: Array(repeating: 0, count: arr1[0].count), count: arr1.count)
+// MARK: - LV1 평균 구하기
+let arr = [1, 2, 3, 4]
 
-for i in 0..<arr3.count {
-    for j in 0..<arr3[0].count {
-        arr3[i][j] = arr1[i][j] + arr2[i][j]
-    }
-}
-print(arr3)
+var k = arr.reduce(0, +)
+print(Double(k) / Double(arr.count))
 
-let k = zip(arr1, arr2).map{zip($0, $1).map{$0 + $1}}
-print(k)
-
- func solution1(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
-    
-     return zip(arr1, arr2).map{zip($0, $1).map{$0 + $1}}
- }
-
-func solution2(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
-    var newArr: [[Int]] = Array(repeating: Array(repeating: 0, count: arr1[0].count), count: arr1.count)
-    for i in 0..<newArr.count {
-        for j in 0..<newArr[0].count {
-            newArr[i][j] = arr1[i][j] + arr2[i][j]
-        }
-    }
-    
-    return newArr
-}
