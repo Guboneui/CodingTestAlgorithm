1c4a0c3f1b79d7a170290cb4900e366eac4a56f5
Guboneui <starku2249@naver.com>
2022-01-04 15:28:15 +0900

    프로그래머스 LV1 직사각형 별찍기

--- CodingTestAlgorithm/main.swift
+++ CodingTestAlgorithm/main.swift
@@ -7,5 +7,19 @@
 
 import Foundation
 
-print("Hello, World!")
+// MARK: - LV1 직사각형 별찍기
+let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+let (a, b) = (n[0], n[1])
 
+// Array joined()를 통한 풀이
+// for _ in 0..<b {
+//     print(Array(repeating: "*", count: a).joined())
+// }
+
+// print의 terminator 디폴트 값 변경을 통한 풀이
+for _ in 1...b {
+    for _ in 1...a {
+        print("*", terminator: "")
+    }
+    print("")
+}
