2c183be4877508044b0995cd547b0905be8796ae
Guboneui <starku2249@naver.com>
2022-02-14 04:22:17 +0900

    백준 10809번 알파벳 찾기

--- CodingTestAlgorithm/main.swift
+++ CodingTestAlgorithm/main.swift
@@ -3809,4 +3809,18 @@ import Foundation
 //}
 
 // MARK: - 백준 1152번 단어의 개수
-print(readLine()!.split(separator: " ").map{String($0)}.count)
+// print(readLine()!.split(separator: " ").map{String($0)}.count)
+
+// MARK: - 백준 10809번 알파벳 찾기
+let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+let a = readLine()!.map{String($0)}
+
+for o in 0...25 {
+  if a.contains(alphabet[o]) {
+      print(a.firstIndex(of:alphabet[o])!,terminator:" ")
+  }else {
+    print("-1",terminator:" ")
+  }
+}
+
