b1bafeb8d4a4added6b41c6ad17c132b939ce31c
Guboneui <starku2249@naver.com>
2022-02-16 18:31:16 +0900

    백준 2960번 에라토스테네스의 체

--- CodingTestAlgorithm/main.swift
+++ CodingTestAlgorithm/main.swift
@@ -4211,32 +4211,62 @@ import Foundation
 
 // MARK: - 백준 6588번 골드바흐의 추측
 
-var aa = Array(repeating: 0, count: 1000001)
-var isGoldBach = false
-for i in 2...1000000{
-    aa[i] = i
+//var aa = Array(repeating: 0, count: 1000001)
+//var isGoldBach = false
+//for i in 2...1000000{
+//    aa[i] = i
+//}
+//for i in 2...1000000{
+//    if aa[i] == 0{continue}
+//    for j in stride(from: i + i, through: 1000000, by: i){
+//        aa[j] = 0
+//    }
+//}
+//
+//while true{
+//    let n = Int(String(readLine()!))!
+//    if n == 0{
+//        break
+//    }
+//    for i in 3..<((n / 2)+1){
+//        if i % 2 == 1 && aa[i] != 0 && aa[n - i] != 0 {
+//            print("\(n) = \(i) + \(n - i)")
+//            isGoldBach = true
+//            break
+//        }
+//    }
+//    if !isGoldBach{
+//        print("Goldbach's conjecture is wrong.")
+//    }
+//    isGoldBach = false
+//}
+
+// MARK: - 백준 2960번 에라토스테네스의 체
+let read: [Int] = readLine()!.split(separator: " ").map{Int($0)!}
+let N: Int = read[0]
+let K: Int = read[1]
+
+var arr: [Int] = Array(repeating: 0, count: N+1)
+for i in 2...N {
+    arr[i] = i
 }
-for i in 2...1000000{
-    if aa[i] == 0{continue}
-    for j in stride(from: i + i, through: 1000000, by: i){
-        aa[j] = 0
+
+var count = 0
+for i in 2...N {
+    if arr[i] == 0 {continue}
+    for j in stride(from: i, through: N, by: i) {
+        if arr[j] == 0 {
+            continue
+        } else {
+            count += 1
+            if count == K {
+                print(j)
+                break
+            }
+            arr[j] = 0
+        }
+        
     }
 }
 
-while true{
-    let n = Int(String(readLine()!))!
-    if n == 0{
-        break
-    }
-    for i in 3..<((n / 2)+1){
-        if i % 2 == 1 && aa[i] != 0 && aa[n - i] != 0 {
-            print("\(n) = \(i) + \(n - i)")
-            isGoldBach = true
-            break
-        }
-    }
-    if !isGoldBach{
-        print("Goldbach's conjecture is wrong.")
-    }
-    isGoldBach = false
-}
+
