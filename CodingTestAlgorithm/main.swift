6c99a0a9a522eec70b8c96ab899c873f9c164660
Guboneui <starku2249@naver.com>
2022-02-21 23:44:02 +0900

    백준 11286번 절댓값 힙

--- CodingTestAlgorithm/main.swift
+++ CodingTestAlgorithm/main.swift
@@ -5360,29 +5360,115 @@ import Foundation
 
 // MARK: - 백준 11279번 최대 힙
 
-let n: Int = Int(readLine()!)!
-var arr: [Int] = []
+//let n: Int = Int(readLine()!)!
+//var arr: [Int] = []
+//
+//for _ in 0..<n {
+//    let temp: Int = Int(readLine()!)!
+//
+//    if temp == 0 {
+//        if arr.isEmpty {
+//            print(0)
+//        } else {
+//            var maxValue: Int = 0
+//            var maxIndex: Int = 0
+//            for i in 0..<arr.count {
+//                if maxValue < arr[i] {
+//                    maxValue = arr[i]
+//                    maxIndex = i
+//                }
+//            }
+//
+//            arr[maxIndex] = 0
+//            print(maxValue)
+//        }
+//    } else {
+//        arr.append(temp)
+//    }
+//}
 
-for _ in 0..<n {
+// MARK: - 백준 11286번 절댓값 힙
+
+let n: Int = Int(readLine()!)!
+var heap = Heap()
+
+for i in 0..<n {
     let temp: Int = Int(readLine()!)!
-    
     if temp == 0 {
-        if arr.isEmpty {
+        if heap.isEmpty() {
             print(0)
         } else {
-            var maxValue: Int = 0
-            var maxIndex: Int = 0
-            for i in 0..<arr.count {
-                if maxValue < arr[i] {
-                    maxValue = arr[i]
-                    maxIndex = i
-                }
-            }
-            
-            arr[maxIndex] = 0
-            print(maxValue)
+            print(heap.remove()!)
         }
     } else {
-        arr.append(temp)
+        heap.add(temp)
     }
+}
+
+
+
+struct Heap {
+    var array: [Int] = []
+    var compare : (Int, Int) -> Bool = {
+        if abs($0) == abs($1) {
+            return $0 > $1
+        } else {
+            return abs($0) > abs($1)
+        }
+    }
+    
+    mutating func add(_ a: Int) {
+        array.append(a)
+        var index: Int = array.count - 1
+        while index > 0, compare(array[(index-1)/2], array[index]) {
+            array.swapAt((index-1)/2, index)
+            index = (index-1)/2
+        }
+    }
+    
+    mutating func remove() -> Int? {
+        if array.isEmpty { return nil }
+        array.swapAt(0, array.count-1)
+        let returnValue = array.removeLast()
+        var index: Int = 0
+        
+        while index<array.count {
+            let compareIndex = index * 2 + 1
+            var changed = false
+            if compareIndex + 1 < array.count {
+                if compare(array[index], array[compareIndex]) {
+                    if compare(array[compareIndex], array[compareIndex+1]) {
+                        array.swapAt(index, compareIndex+1)
+                        index = compareIndex + 1
+                        changed = true
+                    } else {
+                        array.swapAt(index, compareIndex)
+                        index = compareIndex
+                        changed = true
+                    }
+                } else if compare(array[index], array[compareIndex+1]) {
+                    array.swapAt(index, compareIndex+1)
+                    index = compareIndex+1
+                    changed = true
+                }
+            } else if compareIndex < array.count {
+                if compare(array[index], array[compareIndex]) {
+                    array.swapAt(index, compareIndex)
+                    index = compareIndex
+                    changed = true
+                }
+            } else {
+                break
+            }
+            if changed == false {
+                break
+            }
+        }
+        return returnValue
+    }
+    
+    func isEmpty() -> Bool {
+        return array.isEmpty
+    }
+    
 }
