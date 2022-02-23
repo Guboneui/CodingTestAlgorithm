4f869bf2ac04f7d778b1572410d0be302edc9387
Guboneui <starku2249@naver.com>
2022-02-24 01:00:04 +0900

    백준 1331번 나이트 투어

--- CodingTestAlgorithm/main.swift
+++ CodingTestAlgorithm/main.swift
@@ -5821,13 +5821,62 @@ import Foundation
 
 // MARK: - 백준 1312번 소수
 
-let read: [Int] = readLine()!.split(separator: " ").map{Int($0)!}
-var initNum: Int = read[0]
-var result: Double = 0.0
+//let read: [Int] = readLine()!.split(separator: " ").map{Int($0)!}
+//var initNum: Int = read[0]
+//var result: Double = 0.0
+//
+//for _ in 0..<read[2] {
+//    initNum = initNum % read[1]
+//    initNum = initNum * 10
+//    result = Double(initNum) / Double(read[1])
+//}
+//print(Int(result))
 
-for _ in 0..<read[2] {
-    initNum = initNum % read[1]
-    initNum = initNum * 10
-    result = Double(initNum) / Double(read[1])
+// MARK: - 백준 1331번 나이트 투어
+
+var visited = Array(repeating: Array(repeating: false, count: 7), count: 7)
+let dx = [1, 2, 2, 1, -1, -2, -2, -1]
+let dy = [2, 1, -1, -2, 2, 1, -1, -2]
+var startX = 0
+var startY = 0
+var preX = 0
+var preY = 0
+for i in 0..<36{
+    let read = readLine()!.map{String($0)}
+    let x = Int(exactly: Character(read[0]).asciiValue!)! - 65
+    let y = Int(read[1])! - 1
+    if i == 0{
+        startX = x
+        startY = y
+    }
+    if visited[x][y]{
+        break
+    }
+    visited[x][y] = true
+    var isEnded = false
+    for k in 0..<dx.count{
+        let aX = preX + dx[k]
+        let aY = preY + dy[k]
+        if aX == x && aY == y{
+            isEnded = true
+        }
+    }
+    if i != 0 && !isEnded{
+        break
+    }
+    if i == 35{
+        for k in 0..<dx.count{
+            let aX = x + dx[k]
+            let aY = y + dy[k]
+            if aX == startX && aY == startY{
+                print("Valid")
+                exit(0)
+            }
+        }
+    }
+    preX = x
+    preY = y
 }
-print(Int(result))
+print("Invalid")
+
+
