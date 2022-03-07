3c3fb000bbad1026e9f1a5299918b63faeb05b1b
Guboneui <starku2249@naver.com>
2022-03-08 05:42:10 +0900

    백준 15664번 N과 M (11)

--- CodingTestAlgorithm/main.swift
+++ CodingTestAlgorithm/main.swift
@@ -7986,28 +7986,52 @@ import Foundation
 //solution([])
 
 // MARK: - 백준 15664번 N과 M (10)
+//let read: [Int] = readLine()!.split(separator: " ").map{Int($0)!}
+//let n: Int = read[0]
+//let m: Int = read[1]
+//let inputArr: [Int] = readLine()!.split(separator: " ").map{Int($0)!}.sorted()
+//var tempDict: [[Int]:Int] = [:]
+//var visited: [Bool] = Array(repeating: false, count: n)
+//func solution(_ arr: [Int]) {
+//    if arr.count == m {
+//        if tempDict[arr] == nil {
+//            tempDict[arr] = 1
+//            arr.forEach{print($0, terminator: " ")}
+//            print()
+//        }
+//        return
+//    }
+//    for i in 0..<n where arr.last ?? 0 <= inputArr[i] {
+//        if visited[i] == false {
+//            visited[i] = true
+//            solution(arr + [inputArr[i]])
+//            visited[i] = false
+//        }
+//    }
+//}
+//
+//solution([])
+
+// MARK: - 백준 15664번 N과 M (11)
 let read: [Int] = readLine()!.split(separator: " ").map{Int($0)!}
 let n: Int = read[0]
 let m: Int = read[1]
 let inputArr: [Int] = readLine()!.split(separator: " ").map{Int($0)!}.sorted()
-var tempDict: [[Int]:Int] = [:]
+var tempDict: [[String]:Int] = [:]
 var visited: [Bool] = Array(repeating: false, count: n)
-func solution(_ arr: [Int]) {
+var result: String = ""
+func solution(_ arr: [String]) {
     if arr.count == m {
         if tempDict[arr] == nil {
             tempDict[arr] = 1
-            arr.forEach{print($0, terminator: " ")}
-            print()
+            result += "\(arr.joined(separator: " "))" + "\n"
         }
         return
     }
-    for i in 0..<n where arr.last ?? 0 <= inputArr[i] {
-        if visited[i] == false {
-            visited[i] = true
-            solution(arr + [inputArr[i]])
-            visited[i] = false
-        }
+    for i in 0..<n {
+        solution(arr + [String(inputArr[i])])
     }
 }
 
 solution([])
+print(result)
