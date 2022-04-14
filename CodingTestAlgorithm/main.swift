02800c10512146bfc838ab2e9c28aa5c1fbe48f2
Guboneui <starku2249@naver.com>
2022-04-14 22:04:15 +0900

    프로그래머스 level2 순위 검색 -> 정확성 + 효율성

--- CodingTestAlgorithm/main.swift
+++ CodingTestAlgorithm/main.swift
@@ -12159,37 +12159,108 @@ import Foundation
 
 // MARK: - 프로그래머스 level2 순위 검색 -> 정확성 + 효율성
 
+//func solution(_ info:[String], _ query:[String]) -> [Int] {
+//    var infoArr: [(Set<String>, Int)] = []
+//    var queryArr: [(Set<String>, Int)] = []
+//
+//    for i in info {
+//        let temp: [String] = i.split(separator: " ").map{String($0)}
+//        var tempSet: Set<String> = Set(temp[0..<temp.count-1])
+//        var tempScore: Int = Int(temp.last!)!
+//        infoArr.append((tempSet, tempScore))
+//
+//    }
+//
+//    for q in query {
+//        var temp: [String] = q.replacingOccurrences(of: "and ", with: "").replacingOccurrences(of: "- ", with: "").split(separator: " ").map{String($0)}
+//        var tempScore: Int = Int(temp.removeLast())!
+//        var tempSet: Set<String> = Set(temp)
+//        queryArr.append((tempSet, tempScore))
+//    }
+//
+//    var result: [Int] = []
+//
+//    for q in queryArr {
+//        var count: Int = 0
+//        for i in infoArr {
+//            if q.0.subtracting(i.0).isEmpty && i.1 >= q.1 { count += 1}
+//        }
+//        result.append(count)
+//    }
+//
+//    return result
+//}
+
 func solution(_ info:[String], _ query:[String]) -> [Int] {
-    var infoArr: [(Set<String>, Int)] = []
-    var queryArr: [(Set<String>, Int)] = []
+    var result: [Int] = []
+    var dict: [String:[Int]] = [:]
     
-    for i in info {
-        let temp: [String] = i.split(separator: " ").map{String($0)}
-        var tempSet: Set<String> = Set(temp[0..<temp.count-1])
-        var tempScore: Int = Int(temp.last!)!
-        infoArr.append((tempSet, tempScore))
+    for text in info {
+        let infos = text.components(separatedBy: .whitespaces)
+        let languages: [String] = [infos[0], "-"]
+        let jobs: [String] = [infos[1], "-"]
+        let careers: [String] = [infos[2], "-"]
+        let soulFoods: [String] = [infos[3], "-"]
+        let score: Int = Int(infos[4])!
+        
+        for lang in languages {
+            for job in jobs {
+                for career in careers {
+                    for food in soulFoods {
+                        let k: String = "\(lang) \(job) \(career) \(food)"
+                        if dict[k] == nil {
+                            dict[k] = [score]
+                        } else {
+                            dict[k]?.append(score)
+                        }
+                    }
+                }
+            }
+        }
+    }
+        
+    
+    for i in dict {
+        let sortedValue = i.value.sorted()
+        dict[i.key] = sortedValue
+    }
+    
+    
+    query.forEach {
+        let newQuery: [String] = $0.components(separatedBy: .whitespaces)
+        
+        let lang: String = newQuery[0]
+        let job: String = newQuery[2]
+        let career: String = newQuery[4]
+        let food: String = newQuery[6]
+        let score: Int = Int(newQuery[7])!
+        
+        let k: String = "\(lang) \(job) \(career) \(food)"
+        
+        if let matchScores = dict[k] {
+            var start = 0
+            var end = matchScores.count
+            while start < end {
+                let mid = (start + end) / 2
+                if matchScores[mid] >= score {
+                    end = mid
+                } else {
+                    start = mid + 1
+                }
+            }
+            
+            result.append(matchScores.count - start)
+        } else {
+            result.append(0)
+        }
         
     }
     
-    for q in query {
-        var temp: [String] = q.replacingOccurrences(of: "and ", with: "").replacingOccurrences(of: "- ", with: "").split(separator: " ").map{String($0)}
-        var tempScore: Int = Int(temp.removeLast())!
-        var tempSet: Set<String> = Set(temp)
-        queryArr.append((tempSet, tempScore))
-    }
     
-    var result: [Int] = []
-
-    for q in queryArr {
-        var count: Int = 0
-        for i in infoArr {
-            if q.0.subtracting(i.0).isEmpty && i.1 >= q.1 { count += 1}
-        }
-        result.append(count)
-    }
-
+    
     return result
 }
 
-solution(["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"],
-         ["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"])
+
+print(solution(["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"],
+         ["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"]))
